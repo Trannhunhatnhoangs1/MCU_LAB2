11a406f5f34308964280195513d459824a1431d1
Trn_nh1 <hoang.trannhunhat@hcmut.edu.vn>
2025-09-29 10:25:54 +0700

    Init for Ex6

---
+++ LAB2/Core/Inc/software_timer.h
@@ -0,0 +1,16 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Sep 29, 2025
+ *      Author: dell pro
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+
+extern int timer1_flag;
+void setTimer1(int duration);
+void timer_run();
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
--- LAB2/Core/Src/main.c
+++ LAB2/Core/Src/main.c
@@ -22,7 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "software_timer.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -163,23 +163,29 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
-	  second++;
-		  if (second >= 60){
-			  second = 0;
-			  minute++;
-		  }
-		  if(minute >= 60){
-			  minute = 0;
-			  hour++;
-		  }
-		  if(hour >= 24){
-			  hour = 0;
-		  }
-		  updateClockBuffer();
-		  HAL_Delay(1000);
+  setTimer1(1000);
+
+   while (1)
+   {
+ 	  /*
+ 	            second++;
+ 				if (second >= 60) {
+ 					   second = 0;
+ 					   minute++;
+ 						   }
+ 				if (minute >= 60) {
+ 					   minute = 0;
+ 						hour++;
+ 						   }
+ 				 if (hour >= 24) {
+ 					   hour = 0;
+ 						   }
+ 				updateClockBuffer();
+ 	 */
+ 	 if(timer1_flag == 1) {
+ 		HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
+ 		setTimer1(2000);
+ 	  }
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -308,22 +314,25 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN 4 */
 
-int counter = 100;
+int counter =100;
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
+	 timer_run();
+
 	if(counter > 0) {
-			counter--;
-			if(counter % 25 == 0) {
-				update7SEG(index_led++);
-				if(index_led >= 4) index_led = 0;
+				counter--;
+				// SWITCH 4 LED7SEG 250ms
+				if(counter % 25 == 0) {
+					update7SEG(index_led++);
+					if(index_led >= 4) index_led = 0;
+				}
+				// DOT BLINK 1s
+				if(counter == 0) {
+					HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
+					counter = 100;
+				}
 			}
-			//LED RED & DOT BLINK 1s
-			if(counter == 0) {
-				HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
-				HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
-				counter = 100;
-			}
-		}
+
 }
 /* USER CODE END 4 */
 
